022e4f4289ba7bf644f3f3b64fd6278c256ad933
emilioaquino <73952813+emilioaquino@users.noreply.github.com>
2020-11-26 00:12:28 +0800

    Add files via upload

---
+++ aquino-emilio-introtopython/Aquino-Emilio-introtopython.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 195482
+# Aquino
+# 2 BS ITE
+
+# In[ ]:
+
+
+first=input("Enter first number")
+print(first)
+
+
+# In[ ]:
+
+
+second=input("Enter second number")
+print(second)
+
+
+# In[ ]:
+
+
+third=input("Enter third number")
+print(third)
+
+
+# In[ ]:
+
+
+print("The average is",((float(first)+float(second)+float(third))/3))
+
