6e0ca384ee6178469b41f01c9ef40ae17db4b167
emilioaquino <73952813+emilioaquino@users.noreply.github.com>
2020-11-26 00:12:32 +0800

    Add files via upload

---
+++ aquino-emilio-lab1/Aquino-Emilio-lab1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 195482
+# Aquino
+# 2 BS ITE
+
+# In[ ]:
+
+
+dollar=input("Enter the amount in US Dollars:")
+print(dollar,"US dollar(s) =",int(float(dollar)*int(50)),"Philippine Pesos.")
+
+
+# In[ ]:
+
+
+a=input("")
+b=input("")
+print(int(int(a)+int(b)))
+print(int(int(b)-int(a)))
+print(int(int(a)*int(b)))
+print(int(int(a)/int(b)))
+print(int(int(a)%int(b)))
+
+
+# In[ ]:
+
+
+weight=input("Enter kg:")
+height=input("Enter cm:")
+print("BMI:",float(weight)/((float(height)/100)**2))
+
